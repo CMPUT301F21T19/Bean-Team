b35b26a9a40c718d224c42aeb401392214573208
loganv90 <60831705+loganv90@users.noreply.github.com>
2021-10-03 19:15:29 -0600

    added colour properly

--- app/src/main/java/com/example/bean_team/Shape.java
+++ app/src/main/java/com/example/bean_team/Shape.java
@@ -1,11 +1,13 @@
 package com.example.bean_team;
 
-abstract class Shape{
+abstract class Shape {
 
     Integer x;
     Integer y;
 
-    public Shape(Integer x,Integer y){
+    public String colour = "blue";
+
+    public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
     }
--- app/src/main/java/com/example/bean_team/Square.java
+++ app/src/main/java/com/example/bean_team/Square.java
@@ -2,8 +2,6 @@ package com.example.bean_team;
 
 public class Square extends Shape {
 
-    public String colour = "blue";
-
     public Square(Integer x, Integer y) {
         super(x, y);
     }
