dd79cf0f2649fc2a07ecb2a0e08c629ad82607f7
Sohaib Khadri <sohaibx@live.ca>
2021-10-03 18:57:06 -0600

    First Team Push

---
+++ app/src/main/java/com/example/bean_team/Shape.java
@@ -0,0 +1,12 @@
+package com.example.bean_team;
+
+abstract class Shape{
+
+    Integer x;
+    Integer y;
+
+    public Shape(Integer x,Integer y){
+        this.x = x;
+        this.y = y;
+    }
+}
