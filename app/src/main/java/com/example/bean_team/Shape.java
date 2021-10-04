3e1f0e93aa04600e06aec40d1c1d51ff5d8737f6
Sohaib Khadri <sohaibx@live.ca>
2021-10-03 19:09:12 -0600

    Update Shape.java

--- app/src/main/java/com/example/bean_team/Shape.java
+++ app/src/main/java/com/example/bean_team/Shape.java
@@ -5,8 +5,11 @@ abstract class Shape{
     Integer x;
     Integer y;
 
-    public Shape(Integer x,Integer y){
+    String color;
+
+    public Shape(Integer x,Integer y, String color){
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
