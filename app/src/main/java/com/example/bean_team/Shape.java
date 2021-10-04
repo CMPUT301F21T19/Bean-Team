48ac7dddc42fff4a8be2a4be2ac23242453859c8
Andrew Culberson <46251362+the-rooster@users.noreply.github.com>
2021-10-03 19:11:03 -0600

    ADDED COLR FIELD!
    WIOEOEOEOEOEOOEOEOEOEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- app/src/main/java/com/example/bean_team/Shape.java
+++ app/src/main/java/com/example/bean_team/Shape.java
@@ -4,9 +4,12 @@ abstract class Shape{
 
     Integer x;
     Integer y;
+    
+    String color;
 
-    public Shape(Integer x,Integer y){
+    public Shape(Integer x,Integer y,String color){
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
